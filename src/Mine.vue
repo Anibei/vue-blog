2d7ca865b1051392b28380d588157030b276c940
Kevin Cai <caizhifei2003@163.com>
2017-09-27 00:11:54 +0800

    vue blog init

---
+++ src/Mine.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <h4>hello Mine vue !</h4>
+  <p>{{msg}}</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  name:'Mine',
+  data () {
+    return {
+      msg: 'hahhahahaS!'
+    }
+  }
+}
+</script>
+
+<style>
+  h4{
+    color:red;
+  }
+</style>
+
+
